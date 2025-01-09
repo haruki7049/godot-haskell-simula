9f9afb84af3551d17b1db3989e675a6903f17a68
haruki7049 <tontonkirikiri@gmail.com>
2025-01-09 23:05:25 +0900

    WIP

--- flake.nix
+++ flake.nix
@@ -17,13 +17,43 @@
 
       perSystem =
         { pkgs, system, ... }:
+        let
+          overlays = [
+            inputs.stacklock2nix.overlay
+            (final: prev: {
+              gdhs-stacklock = final.stacklock2nix {
+                stackYaml = ./stack.yaml;
+                baseHaskellPkgSet = final.haskell.packages.ghc8107;
+
+                additionalDevShellNativeBuildInputs = stacklockHaskellPkgSet: [
+                  final.stack
+                  final.nil
+                ];
+
+                additionalHaskellPkgSetOverrides = hfinal: hprev: {
+                  lens = final.haskell.lib.compose.dontCheck hprev.lens;
+                };
+
+                all-cabal-hashes = final.fetchFromGitHub {
+                  owner = "commercialhaskell";
+                  repo = "all-cabal-hashes";
+                  rev = "df4fd6587f7e97d8170250ba4419f2cb062736c4";
+                  hash = "sha256-n6VzAT87v2DrMNDsmJXYMTf1a2hhtyx/V5CJkix0cYk=";
+                };
+              };
+            })
+          ];
+        in
         {
-          devShells.default = pkgs.mkShell {
-            packages = [
-              pkgs.nil
-              pkgs.stack
-            ];
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system overlays;
           };
+
+          packages = {
+            default = pkgs.gdhs-stacklock.pkgSet.godot-haskell;
+          };
+
+          devShells.default = pkgs.gdhs-stacklock.devShell;
         };
     };
 }
